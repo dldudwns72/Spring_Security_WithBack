ac61cc286214ca1876ac5f12ea09f17082ff671a
LeeYoungJun <dldudwns72@gmail.com>
2021-02-20 01:37:30 +0900

    account 수정

---
+++ demo-spring-security-form/src/main/java/me/yjlee/demospringsecurityform/account/AccountService.java
@@ -0,0 +1,43 @@
+package me.yjlee.demospringsecurityform.account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService implements UserDetailsService {
+// UserDetailsService는 InMemory가 아닌 DAO를 통해서 저장소에서 유저 정보를 가져오는 인터페이스
+
+    // TODO {noop}123
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    // username을 받아서 user 정보를 꺼내온다.
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // create mehtod 단축기 뭐지;
+        Account account = accountRepository.findByUsername(username);
+        if (account == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return User.builder()
+                .username(account.getUsername())
+                .password(account.getPassword())
+                .roles(account.getRole())
+                .build();
+    }
+
+    public Account createNew(Account account){
+        // account.setPassword("{noop}" + account.getPassword()); // password 인코딩
+        account.encodePassword(passwordEncoder);
+        return this.accountRepository.save(account);
+    }
+}
---
+++ demo-spring-security-form/src/main/java/me/yjlee/demospringsecurityform/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package me.yjlee.demospringsecurityform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// SpringWebSecurity 설정
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // 요청에 대한 인가
+        http.authorizeRequests()
+                .mvcMatchers("/", "/info","/account/**").permitAll() // 권한 상관없이 실행, /** 하면 뒤에 내용 다 허용
+                .mvcMatchers("/admin").hasRole("ADMIN") // ADMIN 이라는 역할을 가질때만 실행
+                .anyRequest().authenticated() // 기타 나머지에 대한 요청은 인증만 하면 접근 가능하다
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+
+//        and()를 통하지 않고 따로 선언 해주어도 된다.
+//        http.formLogin(); // login, loginout 기능
+//        http.httpBasic(); // http 통신 지원
+    }
+
+    // application.properties를 통하여 user 이름, 비밀번호, 권한 을 설정할 수 있다. But 유저 정보를 하나만 가질 수 있고 소스에 정보를 담고있어서 안좋음
+    // PW 설정 : {noop} : 암호화를 하지 않음 , {암호화(인코딩) 방식}, In Memory 방식
+    // User 설정
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("YJLee").password("{noop}123").roles("USER").and()
+//                .withUser("admin").password("{noop}!@#").roles("ADMIN");
+//    }
+}
