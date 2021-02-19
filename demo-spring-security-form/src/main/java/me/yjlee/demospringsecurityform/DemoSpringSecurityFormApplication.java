bc0e1f19a6b04c47e24769abdccc63701ea49d1b
LeeYoungJun <dldudwns72@gmail.com>
2021-02-20 01:25:24 +0900

    Password Encoder 방식 설정

--- demo-spring-security-form/src/main/java/me/yjlee/demospringsecurityform/DemoSpringSecurityFormApplication.java
+++ demo-spring-security-form/src/main/java/me/yjlee/demospringsecurityform/DemoSpringSecurityFormApplication.java
@@ -2,11 +2,24 @@ package me.yjlee.demospringsecurityform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoSpringSecurityFormApplication {
 
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		// 5버전 이후 Deprecated, 인코딩 방식 지정
+//		return NoOpPasswordEncoder.getInstance();
+		// 5버전 이후 권장, 다양한 인코딩 지원
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
 	}
 
