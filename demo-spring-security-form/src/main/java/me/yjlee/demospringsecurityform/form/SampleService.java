d15a21256d1e2ff5632009c2059fcab0f599ffc8
LeeYoungJun <dldudwns72@gmail.com>
2021-02-27 19:13:23 +0900

    SecurityContextHolder에 인증된 유저의 정보가 담김

--- demo-spring-security-form/src/main/java/me/yjlee/demospringsecurityform/form/SampleService.java
+++ demo-spring-security-form/src/main/java/me/yjlee/demospringsecurityform/form/SampleService.java
@@ -5,6 +5,7 @@ import me.yjlee.demospringsecurityform.account.AccountContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -17,9 +18,14 @@ public class SampleService {
 //        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 //        Object credentials = authentication.getCredentials();
 //        boolean authenticated = authentication.isAuthenticated();
-        Account account = AccountContext.getAccount();
+
+//        Account account = AccountContext.getAccount();
+
+        // 인증이 된 사용자의 정보를 SecurityContextHolder 를 사용하여 ThreadLocal 에 저장할 수있다
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         System.out.println("--------------------------");
-        System.out.println(account.getUsername());
+        System.out.println(userDetails.getUsername());
 
     }
 }
